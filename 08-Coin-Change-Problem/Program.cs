8ab502e2afc67cb489b5cad07d19c5932769a6e4
Reda Hilal <reda0x04@gmail.com>
2025-04-26 20:24:54 +0400

    coin change

---
+++ 08-Coin-Change-Problem/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    public static List<int> MinCoins(List<int> coinCategories, int amount)
+    {
+        coinCategories = coinCategories.OrderByDescending(c => c).ToList();
+
+        List<int> result = new List<int>();
+
+        foreach (int coinCategory in coinCategories)
+        {
+            while (amount >= coinCategory)
+            {
+                amount -= coinCategory;
+                result.Add(coinCategory);
+            }
+
+        }
+        return result;
+    }
+
+
+    static void Main(string[] args)
+    {
+        List<int> CoinCategories = new List<int> { 1, 5, 10, 20, 50, 100 };
+
+        int amount = 69;
+
+        List<int> result = MinCoins(CoinCategories, amount);
+
+        Console.WriteLine($"Coins used to form the total Amount: {amount}");
+        for (int i = 0; i < result.Count; i++)
+            Console.Write(result[i] + " ");
+        Console.WriteLine("\nTotal Coins: " + result.Count);
+
+
+
+    }
+}
